b2711a8fbb83e355b60495040ef91a76124f18f7
sezalcru <cesarcruz91@gmail.com>
2015-03-27 13:59:07 -0400

    Change tests to use co-mocha

--- api/test/controller/object.test.js
+++ api/test/controller/object.test.js
@@ -6,72 +6,72 @@ var supertest = require('supertest-as-promised'),
     expect = require('chai').expect,
     utilities = require('../../utilities'),
     app = utilities.server;
-request = supertest(app.callback()),
+    request = supertest(app.callback()),
     data = require('./dummy'),
     Artifact = utilities.models['Object'],
     Content = utilities.models['Content'],
     ObjectContent = utilities.models['ObjectContent'];
+var id,
+    contentOneId,
+    contentTwoId;
 
 describe('Object', function() {
 
-    var id, contentOneId, contentTwoId;
 
     //TODO: Create a Museum object, insert into the database
-    before(function() {
-        Artifact.create(data.object).then(function(result){
-            id = result.dataValues.id;
-            return Content.create(data.contentOne);
-        })
-        .then(function(result){
-            contentOneId = result.dataValues.id;
-            return Content.create(data.contentTwo)
-            })
-        .then(function(result){
-            contentTwoId = result.dataValues.id;
-            return ObjectContent.create({ ObjectId : id, ContentId : contentOneId});
-            })
-        .then(function(result){
-             return ObjectContent.create({ ObjectId : id, ContentId : contentTwoId});
-            })
-        .catch(function(err){
-            throw err;
-            });
+    before(function* () {
+
+        id = (yield Artifact.create(data.object)).dataValues.id;
+
+        contentOneId = (yield Content.create(data.contentOne)).dataValues.id;
+
+        contentTwoId = (yield Content.create(data.contentTwo)).dataValues.id;
+
+        yield ObjectContent.create({ ObjectId : id, ContentId : contentOneId});
+
+        yield ObjectContent.create({ ObjectId : id, ContentId : contentTwoId});
+
 
     });
 
-    describe('GET /object/' + id, function(){
-        it('should return an ok status', function() {
-            return request.get('/object/1') .expect(200);
+    describe('GET /object', function(){
+        it('should return an ok status', function*() {
+            var res = yield request.get('/object/' + id).expect(200);
         });
 
-        it('should return an object', function() {
-            return request.get('/object/' + id).then(function(result){
-               expect(result.body).to.be.a('object');
-            });
+        it('should return an json with object and its content', function*() {
+            var res = yield request.get('/object/' + id)
+            expect(res.body).to.be.a('object');
+
+            expect(res.body.object).to.be.a('object');
+
+            expect(res.body.content).to.be.a('array');
         });
 
-        it('should return have a title attribute', function(){
-            return request.get('/object/' + id, function(result) {
-                expect(result.title).to.be.a('string').and.be.equal(data.object.title);
-            });
+        it('should return have a title attribute', function*(){
+            var res = yield request.get('/object/' + id)
+
+            expect(res.body.object.title).to.be.a('string').and.be.equal(data.object.title);
+
         });
 
-        it('should return a description attribute', function() {
-           return request.get('/object/' + id, function(result){
-                expect(result.description).to.be.a('string').and.be.equal(data.object.description);
-           });
+        it('should return a description attribute', function*() {
+            var res = yield request.get('/object/' + id);
+            expect(res.body.object.description).to.be.a('string').and.be.equal(data.object.description);
         });
 
-        it('should have two associated contents', function(){
-           return request.get('/object/'+id, function(result) {
-                expect(result.content).to.be.a('string');
-           });
+        it('should have two associated contents', function*(){
+            var res = yield request.get('/object/'+id);
+            expect(res.body.content).to.be.a('array');
+
+            expect(res.body.content.length).to.be.equal(2);
+
         });
     });
 
-    after(function() {
-        Artifact.destroy({ where : { title : data.object.title}});
-        Content.destroy({ where : { title : data.contentOne.title}});
-        Content.destroy({ where : { title : data.contentTwo.title}});
+    after(function*() {
+        yield Artifact.destroy({ where : { title : data.object.title}});
+        yield Content.destroy({ where : { title : data.contentOne.title}});
+        yield Content.destroy({ where : { title : data.contentTwo.title}});
     });
 });
