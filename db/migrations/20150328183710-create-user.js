0413d68bffc72bf5fce817f82bd801b8ea11621d
sezalcru <cesarcruz91@gmail.com>
2015-03-28 18:40:29 -0400

    User migration

---
+++ db/migrations/20150328183710-create-user.js
@@ -0,0 +1,59 @@
+"use strict";
+
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    migration.createTable('Users', {
+        id : {
+            type : DataTypes.INTEGER,
+            allowNull : false,
+            primaryKey : true,
+            autoIncrement : true
+        },
+        email : {
+            type : DataTypes.STRING,
+            allowNull : false
+        },
+        firstName : {
+            type : DataTypes.STRING
+        },
+        lastName : {
+            type : DataTypes.STRING
+        },
+        gender : {
+            type : DataTypes.ENUM('male', 'female')
+        },
+        age_range : {
+            type : DataTypes.STRING
+        },
+        points : {
+            type : DataTypes.BIGINT,
+            defaultValue : 0,
+            allowNull : false
+        },
+        active : {
+            type : DataTypes.BOOLEAN,
+            defaultValue : true
+        },
+        facebook_id : {
+            type : DataTypes.STRING
+        },
+        facebook_link : {
+            type : DataTypes.STRING(1000)
+        },
+        createdAt : {
+            type : DataTypes.DATE,
+            allowNull : false
+        },
+        updatedAt : {
+            type : DataTypes.DATE,
+            allowNull : false
+        }
+    }, {
+        freezeTableName : true
+    }).complete(done);
+  },
+
+  down: function(migration, DataTypes, done) {
+    migration.droptTable('Users').complete(done);
+  }
+};
