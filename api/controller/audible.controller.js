031ec3b1d2841ab2bbd70a3cae21cbe7018502f7
sezalcru <cesarcruz91@gmail.com>
2015-03-31 18:10:37 -0400

    Audible controller

---
+++ api/controller/audible.controller.js
@@ -0,0 +1,67 @@
+/**
+ * Created by cesarcruz on 3/31/15.
+ */
+
+var middleware = require('../middleware'),
+    Router = require('koa-router');
+
+module.exports = function() {
+    var loadModels = middleware.loadModel();
+
+    var audibleController = new Router()
+        .get('/artifact/audible/:id', loadModels, index)
+        .get('/audible/:id', loadModels, show);
+
+    return audibleController.routes();
+}
+
+
+function *index() {
+    var audibles,
+        id = parseInt(this.params.id);
+
+    if(!id || id === NaN){
+        this.throw('Invalid Parameters', 400);
+    }
+
+    try {
+        audibles = yield this.sequelize.query('select * from "Audibles" where "id" in (select id from "ArtifactAudibles" where "ArtifactId" = '+ id +');', {type: this.sequelize.QueryTypes.SELECT})
+    } catch(err){
+        this.throw(err.message, err.status || 500);
+    }
+
+    if(!audibles || audibles.length < 1){
+        this.throw('Not Found', 404);
+    }
+
+    this.status = 200;
+
+    this.body = { audibles : audibles };
+}
+
+function *show(){
+    var audible,
+        id = parseInt(this.params.id);
+
+    if(!id || id === NaN){
+        this.throw('Invalid Parameters', 400);
+    }
+
+    try {
+        audible = yield this.models['Audible'].find({
+            where : {
+                id : id
+            }
+        });
+    } catch(err){
+        this.throw(err.message, err.status || 500);
+    }
+
+    if(!audible) {
+        this.throw('Not Found', 404);
+    }
+
+    this.status = 200;
+
+    this.body = audible;
+}
