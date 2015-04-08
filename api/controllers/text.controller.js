0929f77a19b4eb8cd9841a3d815ab2643c5a414a
cesarcruz <cesarcruz91@gmail.com>
2015-04-08 00:50:19 -0400

    Added create, update and delete methods to the Text controller, also fixed some bugs in the image controller.

--- api/controllers/text.controller.js
+++ api/controllers/text.controller.js
@@ -3,7 +3,8 @@
  */
 
 var middleware = require('../middleware'),
-    Router = require('koa-router');
+    Router = require('koa-router'),
+    koaBody = require('koa-better-body')();
 
 module.exports = function(){
 
@@ -11,7 +12,10 @@ module.exports = function(){
 
     var archiveController = new Router()
         .get('/artifact/archive/:id', loadModels, index)
-        .get('/archive/:id', loadModels, show);
+        .get('/archive/:id', loadModels, show)
+        .post('/archive', koaBody, loadModels, create)
+        .put('/archive/:id', koaBody, loadModels, edit)
+        .delete('/archive/:id', loadModels, destroy);
 
     return archiveController.routes();
 }
@@ -72,4 +76,101 @@ function *show() {
     this.status = 200;
 
     this.body = archive;
+}
+
+/**
+ * Create an instance of Archives
+ * Payload: title, description, link, ArtifactId
+ */
+function *create(){
+    var archive = this.request.body.fields,
+        Archive = this.models['Text'],
+        ArtifactArchive = this.models['ArtifactText'],
+        archiveId;
+
+    if(!archive) {
+        this.throw('Bad Request', 400);
+    }
+
+    try {
+        yield this.sequelize.transaction(function(t) {
+            return Archive.create(archive, {transaction : t}).then(function(archive){
+                archiveId = archive.id;
+            });
+        });
+
+        yield this.sequelize.transaction(function(t) {
+            return ArtifactArchive.create({
+                TextId : archiveId,
+                ArtifactId : archive.ArtifactId
+            }, {transaction : t});
+        });
+
+    } catch (err){
+        this.throw(err.message, err.status || 500);
+    }
+
+    this.status = 200;
+}
+
+/**
+ * Update an instance of Archive
+ * Payload: title, description, link, ArtifactId
+ * Note: Only attributes included in the Payload will be updated.
+ */
+function *edit(){
+    var archive = this.request.body.fields,
+        id = this.params.id,
+        result,
+        Archive = this.models['Text'];
+
+    if(!archive) {
+        this.throw('Bad Request', 400);
+    }
+
+    try {
+        result = yield this.sequelize.transaction(function (t){
+            return Archive.update(archive, {
+                where : {
+                    id  : id
+                }
+            }, {transaction : t});
+        });
+    } catch(err) {
+        this.throw(err.message. err.status || 500);
+    }
+
+    if(!result){
+        this.throw('Not Found', 404);
+    }
+
+    this.status = 200;
+}
+
+/**
+ * Destroy an instance of Archive
+ * Parameter: id --> Archive Id
+ */
+function *destroy(){
+    var id = this.params.id,
+        Archive = this.models['Archive'],
+        result;
+
+    try {
+        yield this.sequelize.transaction(function(t) {
+            return Archive.destroy({
+                where : {
+                    id : id
+                }
+            }, { transaction : t});
+        });
+    } catch(err) {
+        this.throw(err.message, err.status || 500);
+    }
+
+    if(!result) {
+        this.throw('Not Found', 404);
+    }
+
+    this.status = 200;
 }
