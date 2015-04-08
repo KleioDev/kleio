cb585302c185cb52d1abe3c248b08849143288f8
cesarcruz <cesarcruz91@gmail.com>
2015-04-08 01:09:15 -0400

    Added update method to museum controller

--- api/controllers/museum.controller.js
+++ api/controllers/museum.controller.js
@@ -14,7 +14,8 @@ module.exports = function(){
 
     var museumController = new Router()
 
-        .get('/museum', loadModels, index);
+        .get('/museum', loadModels, index)
+        .put('/museum/:id', koaBody, loadModels, edit);
 
     return museumController.routes();
 }
@@ -41,4 +42,38 @@ function *index(){
     this.status = 200;
 
     this.body = museum[0];
+}
+
+/**
+ * Update an instance of Museum
+ * Parameter: id --> Museum Id
+ * Payload: name, description, terms, about, hoursOfOperation, socialMediaLinks, phone, extension, image, location, email
+ */
+function *edit(){
+    var museum = this.request.body.fields,
+        Museum = this.models['Museum'],
+        id = this.params.id,
+        result;
+
+        if(!museum) {
+            this.throw('Bad Request', 400);
+        }
+
+        try {
+            result = yield this.sequelize.transaction( function (t) {
+                return Museum.update(museum, {
+                    where : {
+                        id : id
+                    }
+                }, { transaction : t});
+            });
+        } catch(err) {
+            this.throw(err.message, err.status || 500);
+        }
+
+        if(!result) {
+            this.throw('Not Found', 404);
+        }
+
+        this.status = 200;
 }
