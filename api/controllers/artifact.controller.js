b0d0f8a08518f0bc1b43b627bb1b3b2e8a8d801d
cesarcruz <cesarcruz91@gmail.com>
2015-04-08 01:44:50 -0400

    Added create, update and delete method to artifact controller

--- api/controllers/artifact.controller.js
+++ api/controllers/artifact.controller.js
@@ -3,7 +3,8 @@
  */
 
 var middleware = require('../middleware'),
-    Router = require('koa-router');
+    Router = require('koa-router'),
+    koaBody = require('koa-better-body')();
 
 /**
  * Handle request related to Artifacts
@@ -14,7 +15,10 @@ module.exports = function(){
 
     var artifactController = new Router()
         .get('/artifact', loadModels, index)
-        .get('/artifact/:id', loadModels, show);
+        .get('/artifact/:id', loadModels, show)
+        .post('/artifact', koaBody, loadModels, create)
+        .put('/artifact/:id', koaBody, loadModels, edit)
+        .delete('/artifact/:id', loadModels, destroy);
 
     return artifactController.routes();
 }
@@ -22,20 +26,25 @@ module.exports = function(){
 /**
  * Get a list of all artifacts
  * The list will be limited to 10 at a time.
- * Query Parameters: Offset, indicates page that is being requests, if ommited will default
+ * Query Parameters: Offset, indicates page that is being requests, if omitted will default
  */
 function *index(){
     var artifacts,
-        offset = this.query.page;
+        offset = this.request.query.offset,
+        limit = this.request.query.limit;
 
     if(!offset || offset < 1){
         offset = 0;
     }
 
+    if(!limit || limit < 1){
+        limit = 25;
+    }
+
     try {
         artifacts = yield this.models['Artifact'].findAll({
             order : '"createdAt" DESC',
-            limit : 10,
+            limit : limit,
             attributes : ['image', 'title', 'description', 'id'],
             offset : offset
             });
@@ -92,5 +101,87 @@ function *show(){
     this.status = 200;
 
     this.body = artifact;
+}
+
+/**
+ * Create an instance of Artifacts
+ * Payload:
+ */
+function *create(){
+    var artifact = this.request.body.fields,
+        Artifact = this.models['Artifact'],
+        result;
+
+    if(!artifact){
+        this.throw('Bad Request', 400);
+    }
+
+    try {
+        result = yield this.sequelize.transaction( function (t) {
+            return Artifact.create(artifact, {transaction : t});
+        });
+    } catch(err) {
+        this.throw(err.message, err.status || 500);
+    }
+
+    if(!result) {
+        this.throw('Not Found', 404);
+    }
+
+    this.status = 200;
+}
+
+/**
+ * Update an instance of Artifact
+ * Payload:
+ * Note: Will only update the available payload attributes
+ */
+function *edit(){
+    var artifact = this.request.body.fields,
+        Artifact = this.models['Artifact'],
+        id = this.params.id,
+        result;
+
+    if(!artifact){
+        this.throw('Bad Request', 400);
+    }
+
+    try {
+        result = yield this.sequelize.transaction( function (t) {
+            return Artifact.update(artifact, { where : { id : id } }, {transaction : t});
+        });
+    } catch(err) {
+        this.throw(err.message, err.status || 500);
+    }
+
+    if(!result) {
+        this.throw('Not Found', 404);
+    }
+
+    this.status = 200;
+}
+
+/**
+ * Destroy an instance of Artifact
+ * Parameter: id --> Artifact Id
+ */
+function *destroy(){
+    var id = this.params.id,
+        Artifact = this.models['Artifact'],
+        result;
+
+    try {
+        result = yield this.sequelize.transaction( function (t) {
+            return Artifact.destroy({ where : { id : id}}, { transaction : t});
+        })
+    } catch(err) {
+        this.throw(err.message, err.status || 500);
+    }
+
+    if(!result) {
+        this.throw('Not Found', 404);
+    }
+
+    this.status = 200;
 }
 
