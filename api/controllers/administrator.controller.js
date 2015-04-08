5a7f6b2cbcc03777d7c4bfa16413554cff613ff8
cesarcruz <cesarcruz91@gmail.com>
2015-04-07 21:25:00 -0400

    Finished Administrator controller, adding update and delete methods.

--- api/controllers/administrator.controller.js
+++ api/controllers/administrator.controller.js
@@ -18,8 +18,8 @@ module.exports = function(){
         .get('/administrator', loadModels, index)
         .get('/administrator/:id', loadModels, show)
         .post('/administrator', koaBody, loadModels, create)
-       // .put('/administrator/:id', loadModels, edit)
-       // .delete('/administrator/:id', loadModels, destroy);
+        .put('/administrator/:id', koaBody, loadModels, edit)
+        .delete('/administrator/:id', loadModels, destroy);
 
     return administratorController.routes();
 }
@@ -86,7 +86,48 @@ function *show(){
 
     this.body = administrator;
 }
+/**
+ * Update an instance of Administrator
+ * Payload: firstName, lastName, phone, email, password
+ * Note: Will only update the parameters present, no other parameter will be affected.
+ */
+function *edit() {
+    var administrator = this.request.body.fields,
+        id = this.params.id,
+        result;
 
+        console.log(administrator);
+
+        if(!administrator){
+            this.throw('Bad Request', 400);
+        }
+
+        if(administrator.password){
+            //Hash password before updating
+            var salt = bcrypt.genSaltSync(10);
+
+            var hash = bcrypt.hashSync(administrator.password, salt);
+
+            administrator.password = hash;
+        }
+
+        try {
+            result = yield this.models['Administrator'].update(administrator, {
+                where : {
+                    id : id
+                }
+            });
+        } catch(err) {
+            this.throw(err.message, err.status || 500);
+        }
+
+        if(!result) {
+            this.throw('Not Found', 404);
+        }
+
+        this.status = 200;
+
+}
 
 
 /**
@@ -121,4 +162,26 @@ function *create(){
 
     this.status = 200;
 
+}
+
+/**
+ * Delete an instance of Administrator
+ */
+function *destroy() {
+    var id = this.params.id,
+        result;
+
+    //TODO: add deleteAt timestamps, don't actually delete the instances
+
+    try {
+        result = yield this.models['Administrator'].destroy({
+            where : {
+                id : id
+            }
+        });
+    } catch(err) {
+        this.throw(err.message, err.status || 500);
+    }
+
+    this.status = 200;
 }
